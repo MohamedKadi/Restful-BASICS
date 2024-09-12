d8ee0fd0cdaa0f60d01af0d4a5e8167b77eb731e
MohamedKadi <gamesmohamed14@gmail.com>
2024-09-12 20:16:00 +0100

    unique id

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed' everytime u call this it will generate a unique id
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
